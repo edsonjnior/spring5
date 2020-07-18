37d15db6df9f788f86a4ef202ee086dc127a11fe
Edson Júnior <edsonjnior@gmail.com>
2020-07-18 13:51:01 -0300

    Commit inicial
    Signed-off-by: Edson Teixeira Carvalho Júnior <edsonteixeira@infoway-pi.com.br>

---
+++ src/main/java/spring5/spring5webapp/model/Author.java
@@ -0,0 +1,35 @@
+package spring5.spring5webapp.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import java.util.Set;
+
+import static javax.persistence.GenerationType.AUTO;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books;
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
---
+++ src/main/java/spring5/spring5webapp/model/Book.java
@@ -0,0 +1,47 @@
+package spring5.spring5webapp.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+import static javax.persistence.GenerationType.AUTO;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private Long id;
+
+    private String title;
+
+    private String isbn;
+
+    private String publisher;
+
+    @ManyToMany
+    @JoinTable(
+            name = "author_book",
+            joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id")
+    )
+    private Set<Author> authors = new HashSet<>();
+
+    public Book(String title, String isbn, String publisher) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+    }
+
+    public Book(String title, String isbn, String publisher, Set<Author> authors) {
+        this.title = title;
+        this.isbn = isbn;
+        this.publisher = publisher;
+        this.authors = authors;
+    }
+}
